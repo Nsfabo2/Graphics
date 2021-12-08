0a796899cb1b48636037f5b3c87a63de14821589
Najla Abothnain <na.abothnain@gmail.com>
2021-12-08 08:10:33 +0300

    Graphicsm

--- Graphics/ViewController.swift
+++ Graphics/ViewController.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 1# structures
+    
+    struct Points{
+        let X: Double
+        let Y: Double
+    }
+    
+    struct Lines{
+        let Start: Points
+        let End: Points
+        //function
+        func LengthOfLines() -> Double{
+                
+        return sqrt(pow((Start.X - End.X), 2) + pow((Start.Y - End.Y), 2))
+            }
+    }
+    
+    struct Triangle{
+        var points: [Points]
+
+        }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         printContent("hi")
     }
+    
 
-
-}
+}//end class
 
